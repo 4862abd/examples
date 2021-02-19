3cc9638758561e802eb0020f621efd2f218f3a68
user1 <user1@DESKTOP-2EG5R88>
2021-02-19 13:41:06 +0900

    변화된 곳!

---
+++ demo/src/demo/TestByP.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class TestByP {
+	String userId;	//
+	String userName;
+	String userPw;
+
+	public String getTest01() {		return userId;	}
+	public void setTest01(String test01) {		this.userId = userId;	}
+	public String getUserName() {		return userName;	}
+	public void setUserName(String userName) {		this.userName = userName;	}
+	public String getUserPw() {		return userPw;	}
+	public void setUserPw(String userPw) {		this.userPw = userPw;	}
+	
+}
